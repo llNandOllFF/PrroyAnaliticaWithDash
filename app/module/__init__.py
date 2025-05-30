08e8df9c8f1f4388737270f639851c549c94b1f3
llNandOll <fere_10_14@hotmail.com>
2025-05-30 02:24:38 -0500

    debug flalse

--- app/module/__init__.py
+++ app/module/__init__.py
@@ -5,4 +5,4 @@ import os
 def iniciar_dash():
     print("Iniciando App Dash")
     host = os.environ.get('DASH_HOST', '127.0.0.1')#variable for Docker
-    app.run_server(host=host,debug=True, port=PORT)
+    app.run_server(host=host,debug=False, port=PORT)
